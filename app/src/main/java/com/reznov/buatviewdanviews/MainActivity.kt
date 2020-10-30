041bd73396e8c5fcd7b5150471addbfdb40915ee
TegasRenovws <gantaizx@gmail.com>
2020-10-31 00:48:11 +0700

    first commit

---
+++ app/src/main/java/com/reznov/buatviewdanviews/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.reznov.buatviewdanviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Hero AOV"
+    }
+}
